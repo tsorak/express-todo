82ab456c381bcdb3819a06868a7cc8f83b7d7183
tsorak <karost0249@gmail.com>
2023-03-09 00:07:29 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Hello World");
+});
+
+app.listen(3000, () => {
+	console.clear();
+	console.log("Server started on port 3000");
+	console.log("http://localhost:3000");
+});
