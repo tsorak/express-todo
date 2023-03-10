461d4a136bd40946cea07c04615dff57fcf82237
tsorak <karost0249@gmail.com>
2023-03-10 14:37:59 +0100

    Add db.ts

---
+++ src/db.ts
@@ -0,0 +1,28 @@
+import Database from "better-sqlite3";
+
+const db = new Database("dev.db", { verbose: console.log });
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL,
+		name TEXT NOT NULL
+	);
+    CREATE TABLE IF NOT EXISTS friends (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        friend_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (friend_id) REFERENCES users(id)
+    );
+    CREATE TABLE IF NOT EXISTS todos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        title TEXT NOT NULL,
+        description TEXT,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    );
+`);
+
+export default db;
